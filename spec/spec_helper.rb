dbd131716b29303faf3e27e457f668b640ba5bfb
Gabriel Reis <gbrielpedot@gmail.com>
2018-05-25 13:59:51 -0300

    added simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,15 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require "simplecov"
+
+SimpleCov.start do
+  SimpleCov.maximum_coverage_drop 0.2
+  SimpleCov.start do
+    add_filter "vendor"
+  end
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
