b1d3d019e0e7ebe4cb8b5b47cd76d9fd50b7ae10
Gabriel Reis <gbrielpedot@gmail.com>
2018-05-25 13:32:04 -0300

    remodeled wallet model

--- lib/wallet.rb
+++ lib/wallet.rb
@@ -1,8 +1,32 @@
-class Wallet
-  attr_reader :currency, :amount
+require_relative 'money'
 
-  def initialize(attributes)
-    @currency = attributes.fetch(:currency)
-    @amount   = attributes.fetch(:amount, 0)
+class Wallet
+  attr_reader :name, *Money::CURRENCY_SYMBOLS
+
+  def initialize(args)
+    @name = args.fetch(:name)
+    initialize_currencies(args)
+  end
+
+  def wallets
+    wallets = Hash[Money::CURRENCY_SYMBOLS.collect(&currency_and_amount_pair)]
+    wallets.reject { |currency, amount| amount.nil? }
+  end
+
+  private
+
+  def initialize_currencies(currencies)
+    Money::CURRENCY_SYMBOLS.each do |currency|
+      amount = currencies[currency]
+
+      next if amount.nil?
+
+      money = Money.new(currency: currency, amount: amount)
+      instance_variable_set("@#{currency}", money)
+    end
+  end
+
+  def currency_and_amount_pair
+    @currency_and_amount_pair ||= -> (k) { [k, send(k)&.human_amount] }
   end
 end
--- spec/lib/wallet_spec.rb
+++ spec/lib/wallet_spec.rb
@@ -3,21 +3,46 @@ require 'wallet'
 
 RSpec.describe Wallet do
   describe '#initialize' do
-    context 'when the currency is provided' do
-      it 'should create a new Wallet' do
-        params = { currency: 'USD' }
-        wallet = Wallet.new(params)
-
-        expect(wallet).to be_a Wallet
-      end
-    end
-
-    context 'when the currency is not provided' do
+    context 'when the name is not provided' do
       it 'should fail' do
-        params = { amount: 42 }
+        params = { brl: 42 }
 
         expect { Wallet.new(params) }.to raise_error(KeyError)
       end
+    end
+
+    context 'when amounts are provided' do
+      let(:params) { { name: 'jon', brl: 42, tst: 2} }
+      let(:wallet) { Wallet.new(params) }
+
+      it 'should create a wallet for each valid one' do
+        brl = Money.new(currency: :brl, amount: 42)
+
+        expect(wallet.brl).to eq brl
+      end
+
+      it 'should return nil for not provided amounts' do
+        expect(wallet.usd).to be_nil
+      end
+
+      it 'should ignore invalid currencies' do
+        expect(wallet).to_not respond_to(:tst)
+      end
+    end
+  end
+
+  describe '#wallets' do
+    let(:params) { { name: 'jon', eur: 42, usd: 71 } }
+    let(:wallet) { Wallet.new(params) }
+    subject { wallet.wallets }
+
+    it 'should include the non nil currencies and their amounts' do
+      expect(subject).to include(usd: 71)
+      expect(subject).to include(eur: 42)
+    end
+
+    it 'should not included empty ones' do
+      expect(subject.keys).to_not include(:brl)
     end
   end
 end
